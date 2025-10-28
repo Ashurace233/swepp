2940bc086c73cd03e7234b53c193f010f1cab211
Ashurace233 <reginaldannobil850@gmail.com>
2025-10-28 07:46:46 +0000

    Update email-config.js

--- email-config.js
+++ email-config.js
@@ -2,7 +2,8 @@
 // This file is loaded before js/app.js in your pages.
 window.EMAIL_CONFIG = {
   userId: 'URPg7uugKsgNe5s32',      // EmailJS public key (from dashboard)
-  serviceId: 'service_2wqd7qm',        // EmailJS service ID
-  templateId: 'template_14r54zp'       // EmailJS template ID
+  serviceId: 'service_g5qpzrn',        // EmailJS service ID
+  templateId: 'template_o0pen9z'       // EmailJS template ID
 };
+
 // emailjs.init will be attempted by js/app.js if available
