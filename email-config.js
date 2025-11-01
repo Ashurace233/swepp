bb96639e2dd989e07b2dbc485bc8667c0421d94f
Ashurace233 <reginaldannobil850@gmail.com>
2025-11-01 15:41:20 +0000

    Add files via upload

--- email-config.js
+++ email-config.js
@@ -1,12 +1,13 @@
 // Replace placeholders with your EmailJS/public key + service/template IDs.
 // This file is loaded before js/app.js in your pages.
 window.EMAIL_CONFIG = {
-  userId: 'gyLf-PcV8iZLVgPzD',        // EmailJS public key (from dashboard)
-  serviceId: 'service_s09rxcd',        // EmailJS service ID
-  templateId: 'template_s5da7m5',     // EmailJS template ID
+  userId: 'URPg7uugKsgNe5s32',        // EmailJS public key (from dashboard)
+  serviceId: 'service_iqvfy0k',        // EmailJS service ID
+  templateId: 'template_mjukwjj',     // EmailJS template ID
+  officeEmail: 'darklord69233@gmail.com', // Office email address
   
   templateVariables: {
-    to_email: '235pgfe@gmail.com',
+    to_email: 'darklord69233@gmail.com',
     subject_prefix: 'New Grant Application',
     from_name: '{{fullName}}',
     from_email: '{{email}}',
@@ -19,5 +20,3 @@ window.EMAIL_CONFIG = {
 console.log('Email configuration loaded:', EMAIL_CONFIG);
 
 // emailjs.init will be attempted by js/app.js if available
-
-
